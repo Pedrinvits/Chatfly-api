49406908ca3300accf85d90af6953182ae5f1557
Pedro Mantovani <pedromantovani26@gmail.com>
2024-09-04 10:00:17 -0300

    feat : add userId in handleConnection

--- src/chat/chat_gateway.ts
+++ src/chat/chat_gateway.ts
@@ -1,26 +1,24 @@
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Socket,Server } from "socket.io";
+import { Socket, Server } from "socket.io";
 
-@WebSocketGateway(3002, { cors : {origin: '*'}})
-export class ChatGateway implements OnGatewayConnection,OnGatewayDisconnect{
+@WebSocketGateway(3002, { cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
-    @WebSocketServer() server : Server;
+    @WebSocketServer() server: Server;
 
     handleConnection(client: Socket, ...args: any[]) {
-        client.emit('user-join',{
-            message : `New user joined the chat ${client.id}`
-        })
-        
+        client.emit('user-join', {
+            message: `New user joined the chat ${client.id}`,
+            userId: client.id
+        });
     }
+
     handleDisconnect(client: Socket, ...args: any[]) {
-        console.log('client disconnected, id = ',client.id);
-        
+        console.log('client disconnected, id : ', client.id);
     }
+
     @SubscribeMessage('New message')
-    handleNewMessage(client : Socket,message : any){
-        // conseguimos capturar as informacoes da pessoa que enviou
-        // console.log(message);
-        // client.emit('reply','this is a reply')
-        this.server.emit('message', message)
+    handleNewMessage(client: Socket, message: any) {
+        this.server.emit('message', { ...message, userId: client.id });
     }
 }
